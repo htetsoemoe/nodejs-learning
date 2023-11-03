b242580175329e0d41c103a461bbb550abba7ef4
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-03 10:25:58 +0630

    mongoDB mongoose intro

---
+++ chapter-13-mongodb-mongoose-intro/controllers/authController.js
@@ -0,0 +1,75 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) {
+        this.users = data
+    }
+}
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) {
+        return res
+            .status(400)
+            .json({ 'message': 'Username and password are required!' });
+    }
+
+    // find user in the db
+    const foundUser = usersDB.users.find(person => person.username === user);
+
+    if (!foundUser) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
+    // Check Password
+    const matchPassword = await bcrypt.compare(pwd, foundUser.password);
+
+    if (matchPassword) {
+        // Get Roles of login user
+        const roles = Object.values(foundUser.roles);
+        // Create JWTs
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRETE,
+            { expiresIn: '30s' }
+        );
+
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRETE,
+            { expiresIn: '1d' }
+        )
+
+        // Saving refreshToken with current user
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            sameSite: 'None',
+            // secure: true, <= no need api testing in thunder client, but use consuming api from browser and production environment
+            maxAge: 24 * 60 * 60 * 1000
+        });
+        res.json({ accessToken });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
